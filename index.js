751870dbf15a37ad599669663d58d420118ac1b6
helibungy <107930410+helibungy@users.noreply.github.com>
2022-06-22 14:04:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ $(function () {
         //})
         //names = names.slice(0, -1);
 
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${name}`;
+        var msg = `予約日：${date}\n人数：${number}\n氏名：${name}`;
         sendText(msg);
 
         return false;
