cc9165b684f6d2d3f4d555b8eec5b9e78caf1893
helibungy <107930410+helibungy@users.noreply.github.com>
2022-06-22 13:04:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ $(function () {
         });
     });
     $(function () {
-        flatpickr($('input[name="date2"]'), {"locale":"ja","enableTime":true,"plugins":[new confirmDatePlugin({})]});
+        //flatpickr($('input[name="date2"]'), {"locale":"ja","enableTime":true,"plugins":[new confirmDatePlugin({})]});
+        $('input[name="date2"]').datetimepicker({
+            allowTimes:[
+              '11:00', '12:00', '13:00', '14:00',
+              '15:00', '16:00', '17:00', '18:00', 
+              '19:00', '20:00', '21:00', '22:00'],
+              format:'Y年m月d日',
+        });
     });
 
     // 参加人数分の氏名欄を生成
