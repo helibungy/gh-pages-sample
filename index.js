3f632f103984a0959c9653a1f17c5fab55b82b5a
helibungy <107930410+helibungy@users.noreply.github.com>
2022-06-22 13:22:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ $(function () {
     });
     $(function () {
         //flatpickr($('input[name="date2"]'), {"locale":"ja","enableTime":true,"plugins":[new confirmDatePlugin({})]});
+        $.datetimepicker.setLocale('ja');
         $('input[name="date2"]').datetimepicker({
+            format: "d/m/Y H:m:s",
             allowTimes:[
               '11:00', '12:00', '13:00', '14:00',
               '15:00', '16:00', '17:00', '18:00', 
-              '19:00', '20:00', '21:00', '22:00'],
+              '19:00', '20:00', '21:00', '22:00']
         });
     });
 
